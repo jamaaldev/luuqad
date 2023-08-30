8575152aebc81155ee8826030e2960216c77d219
jamaalcoder <kabarojamaal@gmail.com>
2023-08-30 02:45:56 +0100

    Step 10 Database

--- components/learn/courses/CoursesCard.tsx
+++ components/learn/courses/CoursesCard.tsx
@@ -1,22 +1,31 @@
+import { AlphaBetTypeValid } from "@/validations/AlphabetIsValid"
 import Image from "next/image"
 
-type Props = {}
+type Props = {
+  course: AlphaBetTypeValid
+}
 
 const CoursesCard = (props: Props) => {
+  const handleClick = (direction: React.SyntheticEvent<HTMLElement>) => {
+    console.log("yes click", direction.currentTarget.dataset.direction)
+  }
   return (
-    <div className='relative w-fit px-12 cursor-pointer  py-10 flex justify-center text-center items-center active:translate-y-[2px] active:translate-z-0 before:active:shadow-none before:content-[""] before:absolute before:shadow-border_b before:shadow-slate-200 before:border-2 before:-left-0 before:-right-0 before:-top-2 before:-bottom-2 before:rounded-2xl before:hover:bg-slate-100 before:-z-10  '>
-      <div>
+    <div
+      onClick={handleClick}
+      data-direction={props.course.Direction}
+      className='relative w-fit px-12 cursor-pointer  py-10 flex justify-center text-center items-center active:translate-y-[2px] active:translate-z-0 before:active:shadow-none before:content-[""] before:absolute before:shadow-border_b before:shadow-slate-200 before:border-2 before:-left-0 before:-right-0 before:-top-2 before:-bottom-2 before:rounded-2xl before:hover:bg-slate-100 before:-z-10  '>
+      <button>
         <Image
-          src={"/svg/englished.svg"}
+          src={`/svg/${props.course.Langauge}.svg`}
           width={100}
           height={100}
-          alt='langauge english'
+          alt={props.course.Langauge}
         />
-        <p>English</p>
+        <p>{props.course.Langauge}</p>
         <span>
-          `${"35"}M {"Learners"}`
+          {"35"}M {"Learners"}
         </span>
-      </div>
+      </button>
     </div>
   )
 }
--- components/learn/courses/CoursesList.tsx
+++ components/learn/courses/CoursesList.tsx
@@ -1,11 +1,15 @@
+import { useGetAlphaBetsQuery } from "@/store/slices/Courses"
 import CoursesCard from "./CoursesCard"
 
 type Props = {}
 
 const CoursesList = (props: Props) => {
+  const { data: alphabetdata } = useGetAlphaBetsQuery("getall")
   return (
-    <div>
-      <CoursesCard />
+    <div className='flex justify-center flex-wrap gap-7 mb-6 mt-11'>
+      {alphabetdata?.map((course) => (
+        <CoursesCard key={course.id} course={course} />
+      ))}
     </div>
   )
 }
