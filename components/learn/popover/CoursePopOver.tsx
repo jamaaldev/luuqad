273f631def9a9c93f66f727ea8dab8f3ecd543ca
jamaalcoder <kabarojamaal@gmail.com>
2023-09-02 16:46:18 +0100

    Step 21 Database

--- components/learn/popover/CoursePopOver.tsx
+++ components/learn/popover/CoursePopOver.tsx
@@ -10,7 +10,7 @@ const CoursePopOver = (props: Props) => {
   return (
     <>
       {props.isopen ? (
-        <div className='grid bg-white  rounded-2 border-2 w-64 h-max mt-3 '>
+        <div className='grid bg-white  rounded-2 border-2 w-64 h-max mt-3 overflow-hidden '>
           <div className='border-b-2 p-3'>
             <h2>My Courses</h2>
           </div>
@@ -24,12 +24,14 @@ const CoursePopOver = (props: Props) => {
             <span>English</span>
           </div>
 
-          <div className='flex items-center gap-3 border-b-1 p-3 cursor-pointer hover:bg-slate-50'>
-            <span className='p-2 bg-slate-100 rounded-lg '>
-              <LuuqadIconX width={15} />
-            </span>
-            <Link href={"/learn/courses"}>Add a New Course</Link>
-          </div>
+          <Link href={"/learn/courses"}>
+            <div className='flex items-center gap-3 border-b-1 p-3 cursor-pointer hover:bg-slate-50 '>
+              <span className='p-2 bg-slate-100 rounded-lg '>
+                <LuuqadIconX width={15} />
+              </span>
+              Add a New Course
+            </div>
+          </Link>
         </div>
       ) : (
         ""
