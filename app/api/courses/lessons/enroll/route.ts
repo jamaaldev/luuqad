41e7ff09982a5196920ecf16ae01f62e1bbfe92f
jamaalcoder <kabarojamaal@gmail.com>
2023-09-03 17:06:01 +0100

    Step 26 Database

--- app/api/courses/lessons/enroll/route.ts
+++ app/api/courses/lessons/enroll/route.ts
@@ -5,20 +5,20 @@ import { NextRequest, NextResponse } from "next/server"
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
     const newLessons: CoursesTypeValid = await req.json()
-    const checkifExist: CoursesTypeValid | null =
+    const UserCourseExist: CoursesTypeValid | null =
       await prisma.userCourses.findFirst({
-        where: { alphabet_id: newLessons.alphabet_id },
+        where: { user_id: newLessons.user_id },
         select: {
           user_id: true,
           alphabet_id: true,
           isSelected: true,
         },
       })
-    if (checkifExist?.alphabet_id === newLessons.alphabet_id) {
+    if (UserCourseExist?.alphabet_id === newLessons.alphabet_id) {
       // You might want to return a proper response in case of an error
       return NextResponse.json(
         {
-          error: "Sorry, the course already exist.",
+          error: "Sorry, the Course already exist.",
         },
         { status: 409 },
       )
@@ -28,7 +28,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
     })
     return NextResponse.json(
       {
-        message: "Successfully added Character",
+        message: "Successfully added newCourse",
       },
       { status: 200 },
     )
