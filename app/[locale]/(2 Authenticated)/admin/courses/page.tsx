0b0c0a2be496a4637f6a2c406c09b60133c499f4
jamaalcoder <kabarojamaal@gmail.com>
2023-09-03 21:47:37 +0100

    Step 31 Database

--- app/[locale]/(2 Authenticated)/admin/courses/page.tsx
+++ app/[locale]/(2 Authenticated)/admin/courses/page.tsx
@@ -9,12 +9,12 @@ const page = (props: Props) => {
         <Link
           href='/admin/courses/alphabets/create'
           className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mb-2 w-2/12'>
-          Create Alphabets
+          Create Alphabet Course
         </Link>
         <Link
           href='/admin/courses/characters/create'
           className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mb-2 w-2/12'>
-          Create Characters
+          Create Character
         </Link>
       </div>
     </div>
