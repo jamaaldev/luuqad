eaf710e0c946ad2f9eeedcf2249f5d2c07f28796
jamaalcoder <kabarojamaal@gmail.com>
2023-09-03 19:06:28 +0100

    Step 29 Database

--- app/api/courses/lessons/enroll/route.ts
+++ app/api/courses/lessons/enroll/route.ts
@@ -5,16 +5,24 @@ import { NextRequest, NextResponse } from "next/server"
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
     const newLessons: CoursesTypeValid = await req.json()
+    // check 2 filter and return value or null
     const UserCourseExist: CoursesTypeValid | null =
       await prisma.userCourses.findFirst({
-        where: { user_id: newLessons.user_id },
+        where: {
+          AND: [
+            { user_id: newLessons.user_id },
+            { alphabet_id: newLessons.alphabet_id },
+          ],
+        },
+
         select: {
           user_id: true,
           alphabet_id: true,
           isSelected: true,
         },
       })
-    if (UserCourseExist?.alphabet_id === newLessons.alphabet_id) {
+
+    if (UserCourseExist) {
       // You might want to return a proper response in case of an error
       return NextResponse.json(
         {
--- validations/CoursesIsValid.tsx
+++ validations/CoursesIsValid.tsx
@@ -3,11 +3,11 @@ import * as yup from "yup"
 export const CoursesSchemaValid = yup.object({
   user_id: yup.number().required(),
   alphabet_id: yup.number().required(),
+  isSelected: yup.number().required(),
 })
 
 export interface CoursesTypeValid
   extends yup.InferType<typeof CoursesSchemaValid> {
   // using interface instead of type generally gives nicer editor feedback
   id?: number
-  isSelected: number
 }
