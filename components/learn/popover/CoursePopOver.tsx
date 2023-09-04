321dc6a291ca26a2d4b90da2dc74a7fcb68a7618
jamaalcoder <kabarojamaal@gmail.com>
2023-09-04 19:40:25 +0100

    Step 33 Database

--- components/learn/popover/CoursePopOver.tsx
+++ components/learn/popover/CoursePopOver.tsx
@@ -1,4 +1,9 @@
 import LuuqadIconX from "@/components/icons/LuuqadIconX"
+import {
+  useGetAlphaBetsQuery,
+  useUpdateUserSelectedMutation,
+} from "@/store/slices/Courses"
+import { useSession } from "next-auth/react"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -7,6 +12,19 @@ type Props = {
 }
 
 const CoursePopOver = (props: Props) => {
+  const { data: courses } = useGetAlphaBetsQuery("getall")
+  const [updateSelectedCourse] = useUpdateUserSelectedMutation()
+  const { data: session, status } = useSession()
+
+  const handleClick = (id: number | undefined) => {
+    updateSelectedCourse({
+      user_id: session?.user?.id,
+
+      isSelected: id,
+    })
+    console.log("🚀 ~ file: CoursePopOver.tsx:14 ~ CoursePopOver ~ id:", id)
+  }
+
   return (
     <>
       {props.isopen ? (
@@ -14,15 +32,20 @@ const CoursePopOver = (props: Props) => {
           <div className='border-b-2 p-3'>
             <h2>My Courses</h2>
           </div>
-          <div className='flex items-center gap-3 border-b-2 p-3 cursor-pointer hover:bg-slate-50'>
-            <Image
-              width={35}
-              height={30}
-              src={"/svg/English.svg"}
-              alt='English'
-            />
-            <span>English</span>
-          </div>
+          {courses?.map((course) => (
+            <div
+              key={course.id}
+              onClick={() => handleClick(course?.id)}
+              className='flex items-center gap-3 border-b-2 p-3 cursor-pointer hover:bg-slate-50'>
+              <Image
+                width={35}
+                height={30}
+                src={`/svg/${course.Langauge}.svg`}
+                alt={course.Langauge}
+              />
+              <span>{course.Langauge}</span>
+            </div>
+          ))}
 
           <Link href={"/learn/courses"}>
             <div className='flex items-center gap-3 border-b-1 p-3 cursor-pointer hover:bg-slate-50 '>
